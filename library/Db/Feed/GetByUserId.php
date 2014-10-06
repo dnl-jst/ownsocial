7e37c72283f95194e6b01d0379fee1b251332b9d
djost <dj@permanent.de>
2014-10-06 20:24:11 +0200

    fixed like-problem

--- library/Db/Feed/GetByUserId.php
+++ library/Db/Feed/GetByUserId.php
@@ -20,14 +20,14 @@ class Db_Feed_GetByUserId extends Core_Query
 				p.modified,
 				u.first_name,
 				u.last_name,
-				IFNULL(u.portrait_file_id, c.value) AS portrait_file_id,
+				IFNULL(u.portrait_file_id, cnfg.value) AS portrait_file_id,
 				IF(IFNULL(ml.user_id, 0) = 0, 0, 1) AS liked,
 				COUNT(al.user_id) AS likes,
 				COUNT(sp.id) AS comments
 			FROM
 				posts p
 			JOIN users u ON u.id = p.user_id
-			JOIN configs c ON c.key = \'default_portrait_id\'
+			JOIN configs cnfg ON cnfg.key = \'default_portrait_id\'
 			LEFT JOIN posts sp ON sp.parent_post_id = p.id
 			LEFT JOIN likes ml ON ml.post_id = p.id AND ml.user_id = ?
 			LEFT JOIN likes al ON al.post_id = p.id
