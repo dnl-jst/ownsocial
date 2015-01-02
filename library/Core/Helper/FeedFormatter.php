ff2ebe1be96f42a8bcfff9ea37e7f2121f7ac1a5
Daniel Jost <daniel.jost@jost-solutions.de>
2015-01-02 12:05:28 +0100

    # feed: transform plain links to a-tags

--- library/Core/Helper/FeedFormatter.php
+++ library/Core/Helper/FeedFormatter.php
@@ -7,7 +7,13 @@ class FeedFormatter
 
 	public static function format($content)
 	{
+		# strip tags
 		$content = strip_tags($content);
+
+		# replace links
+		$content = preg_replace('~(http|https)\:\/\/[^\s]+~', '<a href="$0" target="_blank">$0</a>', $content);
+
+		# newlines to br
 		$content = nl2br($content);
 
 		return $content;
